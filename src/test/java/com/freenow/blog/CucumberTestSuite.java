bc22f2f64dfabf1fc7331f5820794a7fb94c42a2
Billy Yang <Billy.Yang@verifone.com>
2021-07-13 15:43:00 -0400

    [billy] Add cuumberTestSuite optional line glue,this line is optional,with or without it ,framework works.

--- src/test/java/com/freenow/blog/CucumberTestSuite.java
+++ src/test/java/com/freenow/blog/CucumberTestSuite.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
     plugin = {"pretty"},
-    features = "src/test/resources/features"
+    features = "src/test/resources/features",
+        glue = {"com/freenow/blog/stepdefinitions"}
 )
 public class CucumberTestSuite {
 
